0e94a0745f19c4675c4979c040e4eb35694a6625
yongbok1102 <yongbok1102@gmail.com>
2016-08-08 19:44:11 +0900

    laplace.cpp
    source code for solving 2D Laplace Equation Using FDM

--- laplace.cpp
+++ laplace.cpp
@@ -8,7 +8,7 @@ using namespace std;
 /*Laplace equation using FDM         */
 /*Boundary conditions are given below*/
 /*u(0,y)=1, u(1,y)=u(x,1)=u(x,0)=0   */
-
+/*-----------------------------------*/
 int main()
 {
   double x[21]; double y[21];
@@ -47,7 +47,7 @@ int main()
         else if(j==20 && i>=1 && i<=19){u[i][j]=0;}
         
         //Gauss-Seidel Iteration
-		    else { u[i][j] = (u[i - 1][j] + u[i + 1][j] + u[i][j - 1] + u[i][j + 1]) / 4.; }
+		else { u[i][j] = (u[i - 1][j] + u[i + 1][j] + u[i][j - 1] + u[i][j + 1]) / 4.; }
         cout<<x[i]<<'\t'<<y[j]<<'\t'<<u[i][j]<<endl;
         }
       }
@@ -55,13 +55,19 @@ int main()
     
     for(int i=1;i<=10;i++){cout<<'-';}cout<<'\n';
     
-    
+   //Printing out the result
    ofstream out;
-   out.open("laplace.txt");
+   out.open("laplace.plt");
+   out<<" TITLE=\"laplace.plt\" "<<endl;
+   out<<"VARIABLES=\"X\",\"Y\",\"T\"\n";
+   out<<"ZONE   F=POINT\n";
+   out<<"I="<<21<<",J="<<21<<endl;
    for(int i=0;i<=20;i++)
    {
       for(int j=0;j<=20;j++)
       {
+         cout.precision(10);
+         out.precision(10);
          cout<<x[i]<<'\t'<<y[j]<<'\t'<<u[i][j]<<endl;
          out<<x[i]<<'\t'<<y[j]<<'\t'<<u[i][j]<<endl;   
       }
